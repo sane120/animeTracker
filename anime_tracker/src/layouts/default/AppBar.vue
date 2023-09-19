afb472803a36ef8bc9597bf281bc7627d53ec2f4
sane120 <67514322+sane120@users.noreply.github.com>
2023-09-19 09:11:07 +0200

    Update AppBar.vue

--- anime_tracker/src/layouts/default/AppBar.vue
+++ anime_tracker/src/layouts/default/AppBar.vue
@@ -1,39 +1,3 @@
-<template>
-<v-app-bar
-        color="teal-darken-4"
-        image="https://picsum.photos/1920/1080?random"
-      >
-      <template v-slot:image>
-          <v-img
-            gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
-          ></v-img>
-        </template>
-
-        <template v-slot:prepend>
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        </template>
-
-        <v-app-bar-title @click="$router.push('/')"
-          style="cursor:pointer"
-          >PLACEHOLDER
-        </v-app-bar-title>
-
-        <v-spacer></v-spacer>
-
-        <v-btn to="/two" icon>
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-
-        <v-btn to="/three" icon>
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-
-        <v-btn icon>
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </v-app-bar>
-</template>
-
 <script lang="ts">
 
 import { defineComponent } from 'vue';
@@ -47,3 +11,37 @@ import { defineComponent } from 'vue';
     }
   })
 </script>
+<template>
+  <v-app-bar
+          color="#C08497">
+          <template v-slot:prepend>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+          </template>
+
+          <v-app-bar-title class="title" @click="$router.push('/')"
+            >Anitrack
+          </v-app-bar-title>
+          <v-spacer></v-spacer>
+          <v-btn to="/two" icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+          <v-btn to="/three" icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+  </template>
+  <style scoped>
+.title {
+  font-family: Roboto;
+  font-size: 36px;
+  color: #FEF5EF;
+  cursor: pointer;
+}
+.title:hover {
+  opacity: .5;
+}
+
+</style>
